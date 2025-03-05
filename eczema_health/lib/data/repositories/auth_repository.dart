5df6ae2941cc0905550e97460bc8355f87bd07e2
Izhaan Salam <izhaan.salam@student.manchester.ac.uk>
2025-03-05 14:12:28 +0000

    Implemented signup user method in auth repo

--- eczema_health/lib/data/repositories/auth_repository.dart
+++ eczema_health/lib/data/repositories/auth_repository.dart
@@ -16,7 +16,29 @@ class AuthRepository {
         password: password,
       );
 
+      if (response.session == null) {
+        throw Exception('Auth failed');
+      }
       // This point means auth was successful
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<AuthResponse> signUpUser({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await _supabase.auth.signUp(
+        email: email,
+        password: password,
+      );
+
+      if (response.session == null) {
+        throw Exception("Sign up failed");
+      }
       return response;
     } catch (e) {
       rethrow;

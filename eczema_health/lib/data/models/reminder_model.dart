5456b7a16fe4e204722ec32c8eefd297e23603fb
Izhaan Salam <izhaan.salam@student.manchester.ac.uk>
2025-03-07 19:14:12 +0000

    Added data model for reminder incl conversion to map and back

---
+++ eczema_health/lib/data/models/reminder_model.dart
@@ -0,0 +1,12 @@
+class ReminderModel {
+  final String id;
+  final String userId;
+  final String title;
+  final String description;
+  final String reminderType;
+  final DateTime dateTime;
+  final List repeatDays;
+  final bool isActive;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+}

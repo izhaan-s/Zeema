6cdef7c1778bcd1c4428c186eb9b31da1beab02c
Izhaan Salam <izhaan.salam@student.manchester.ac.uk>
2025-05-17 22:59:57 +0100

    Had to create tuple w options (not sure why tuples pub isn't working)

--- eczema_health/lib/data/repositories/cloud/analysis_repository.dart
+++ eczema_health/lib/data/repositories/cloud/analysis_repository.dart
@@ -1,7 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:eczema_health/data/models/analysis_models.dart';
-import 'package:tuple/tuple.dart';
+
+class Tuple<T1, T2, T3, T4> {
+  final T1 item1;
+  final T2 item2;
+  final T3 item3;
+  final T4 item4;
+
+  Tuple(this.item1, this.item2, this.item3, this.item4);
+}
 
 class AnalysisRepository {
   final String baseUrl =

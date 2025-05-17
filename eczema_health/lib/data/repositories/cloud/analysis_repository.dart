215817e30221b4aa21c89d89cf511f7d9d69e96e
Izhaan Salam <izhaan.salam@student.manchester.ac.uk>
2025-05-17 21:35:41 +0100

    Test file for flare POST request I am so cooked

--- eczema_health/lib/data/repositories/cloud/analysis_repository.dart
+++ eczema_health/lib/data/repositories/cloud/analysis_repository.dart
@@ -3,21 +3,26 @@ import 'package:http/http.dart' as http;
 import 'package:eczema_health/data/models/analysis_models.dart';
 
 class AnalysisRepository {
-  final String baseUrl = 'http://10.0.2.2:8000'; // change for production
+  final String baseUrl =
+      'http://10.0.2.2:8000'; // change when I host to render!!!
 
   Future<List<FlareCluster>> getFlareClusters(
       List<Map<String, dynamic>> symptomEntries) async {
+    final body = jsonEncode(symptomEntries);
+    print('Request body: $body'); // Debug print
+
     final response = await http.post(
       Uri.parse('$baseUrl/flare/clusters'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(symptomEntries),
+      body: body,
     );
 
     if (response.statusCode == 200) {
       final List<dynamic> jsonList = jsonDecode(response.body);
       return jsonList.map((e) => FlareCluster.fromJson(e)).toList();
     } else {
-      throw Exception('Failed to get flare clusters');
+      print('Error response: ${response.body}'); // Debug print
+      throw Exception('Failed to get flare clusters: ${response.body}');
     }
   }
 }
